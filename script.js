f6e98cba25f858ab7018775227ee1d08cf609f1c
c0mr8 <rhgftyurkeyutghjd@gmail.com>
2024-07-10 16:59:46 +0500

    Added section with reviews

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+  function scrollLeft() {
+    const carousel = document.querySelector('.carousel');
+    const itemWidth = carousel.querySelector('.carousel-item').clientWidth;
+    carousel.scrollBy({ left: -itemWidth, behavior: 'smooth' });
+  }
+  function scrollRight() {
+    const carousel = document.querySelector('.carousel');
+    const itemWidth = carousel.querySelector('.carousel-item').clientWidth;
+    carousel.scrollBy({ left: itemWidth, behavior: 'smooth' });
+  }
